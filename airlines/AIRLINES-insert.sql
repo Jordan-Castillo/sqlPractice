1919aef4e029d364374679092159175ac0772fe8
Jordan Castillo <jtcastil@calpoly.edu>
2017-10-22 18:07:21 -0700

    Made 2 insert files

---
+++ airlines/AIRLINES-insert.sql
@@ -0,0 +1,11 @@
+/*\
+	Name: Jordan Castillo
+	Date: 10 / 21 / 17
+	Email: jtcastil@calpoly.edu
+\*/
+
+SOURCE AIRLINES-build-airlines.sql
+
+SOURCE AIRLINES-build-airports100.sql
+
+SOURCE AIRLINES-build-flights.sql
---
+++ bakery/BAKERY-insert.sql
@@ -0,0 +1,23 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : BAKERY-insert.sql
+
+* Purpose :
+
+* Creation Date : 21-10-2017
+
+* Last Modified : Sat 21 Oct 2017 04:14:54 PM PDT
+
+* Created By : Jordan Castillo
+
+* Email: jtcastil@calpoly.edu 
+
+_._._._._._._._._._._._._._._._._._._._._.*/
+
+SOURCE BAKERY-build-customers.sql
+
+SOURCE BAKERY-build-goods.sql
+
+SOURCE BAKERY-build-receipts.sql
+
+SOURCE BAKERY-build-items.sql
