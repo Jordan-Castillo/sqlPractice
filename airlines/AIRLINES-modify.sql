cf625f69e98b30d2e80f1104554f6e4a3f31afe1
Jordan-Castillo <jtcastil@calpoly.edu>
2017-10-23 11:40:56 -0700

    attempting airlines modify

--- airlines/AIRLINES-modify.sql
+++ airlines/AIRLINES-modify.sql
@@ -6,7 +6,7 @@
 
 * Creation Date : 22-10-2017
 
-* Last Modified : Sun 22 Oct 2017 06:41:20 PM DST
+* Last Modified : Mon 23 Oct 2017 11:40:42 AM DST
 
 * Created By :  Jordan Castillo
 
@@ -17,4 +17,12 @@ _._._._._._._._._._._._._._._._._._._._._.*/
 --2) Remove from the flights database all flights except for those to and from 'AKI'
 DELETE FROM flightsList
 WHERE (sourceAirport != 'AKI') AND (destAirport != 'AKI');
+--3) For all flights not operated by Continental, AirTran, or Virgin, increase flightnum by 2000
+UPDATE flightsList
+JOIN airlinesList ON flightsList.airlineID = airlinesList.airlineID 
+SET flightsList.flightNum = flightNum + 2000 
+WHERE (airlinesList.airlineName != Continental Airlines) 
+AND (airlinesList.airlineName != AirTran Airways) 
+AND (airlinesList.airlineName != Virgin America);
+
 
