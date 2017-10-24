8bb962c109656bef402e56f2f1182adf33df99b9
Jordan-Castillo <jtcastil@calpoly.edu>
2017-10-24 13:04:49 -0700

    Created INN-insert.sql

---
+++ inn/INN-insert.sql
@@ -0,0 +1,17 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : INN-insert.sql
+
+* Purpose :
+
+* Creation Date : 24-10-2017
+
+* Last Modified : Tue 24 Oct 2017 01:03:51 PM DST
+
+* Created By :  Jordan Castillo
+
+* Email : jtcastil@calpoly.edu
+_._._._._._._._._._._._._._._._._._._._._.*/
+SOURCE INN-build-Rooms.sql
+
+SOURCE INN-build-Reservations.sql
